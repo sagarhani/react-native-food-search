9b48788c6b41e2c852ac41aca3b6929fceba81b8
sagarhani <sagarhani33@gmail.com>
2019-08-14 09:21:26 +0530

    🐛 Removed camera screen

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import SearchScreen from "./src/screens/SearchScreen";
 import ResultsShowScreen from "./src/screens/ResultsShowScreen";
-import CameraScreen from "./src/screens/CameraScreen";
 
 const navigator = createStackNavigator(
   {
     Search: SearchScreen,
-    ResultsShow: ResultsShowScreen,
-    Camera: CameraScreen
+    ResultsShow: ResultsShowScreen
   },
   {
     initialRouteName: "Search",
